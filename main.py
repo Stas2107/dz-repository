2f2fcf89997ef2550f800b5b89caeffea4b7b686
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-03-27 19:29:35 +0400

    add new class

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-print("привет Стас")
-print("Hello World")
-print("Это вторая строка")
+class Warior():
+    def init(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
 
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance = + 2
+
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power = + 1
+
+    def hit(self):
+        print(f"{self.name} бьет кого-то")
+        self.endurance = - 6
+
+    def walk(self):
+        print(f"{self.name} гуляет")
+
+    def walk(self):
+        print(f"имя воина - {self.name}”)
+        print(f"цвет волос воина - {self.hair_color}”)
+        print(f"сила воина - {self.power}”)
+        print(f"выносливость воина - {self.endurance}”)
